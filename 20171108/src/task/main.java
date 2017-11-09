29033aca6c331bc4b6314d4df8d101a9081a71ce
blackbike007 <473523122@qq.com>
2017-11-09 13:37:53 +0800

    text-3

---
+++ 20171108/src/task/main.java
@@ -0,0 +1,21 @@
+package task;
+
+public class main {
+	protected int age1;
+	private int age2;
+	int age3;
+	public int age4;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		main p=new main();
+		p.age1=1;
+		p.age2=2;
+		p.age3=3;
+		p.age4=4;
+		System.out.print(p.age1);
+		System.out.print(p.age2);
+		System.out.print(p.age3);
+		System.out.print(p.age4);
+	}
+
+}
