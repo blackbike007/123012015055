bc6ccc7d04b36772bde19cff58788694986c5226
blackbike007 <473523122@qq.com>
2017-12-14 17:07:55 +0800

    add 20171103

---
+++ 20171103/src/fjnu/java/scheduling/SjfSchedulingTest.java
@@ -0,0 +1,11 @@
+package fjnu.java.scheduling;
+
+public class SjfSchedulingTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SjfScheduling sjf = new SjfScheduling("input.txt", 2);
+		sjf.execute();
+	}
+
+}
